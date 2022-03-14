d3822e02382a907dab1ec22647b7cb27ded712f0
Shakkir Moulana <shakkirmln@gmail.com>
2022-03-14 16:59:41 +0530

    Refactored the code

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+// NOTE: Page title should be unique. And inside a page section title should be unique.
 var pageData = [
   {
     "Page 1 title": [
       {
-        "Section 1 tile": "Section 1 content sec 2",
+        "Section 1 tile": "abb Section 1 content sec2  abb",
       },
       {
         "Section 2 tile": "Section 2 content",
@@ -15,137 +16,90 @@ var pageData = [
         "Section 1 tile": "Section 1 content",
       },
       {
-        "Section 2 tile": "Section 2 content",
+        "Section 2 tile": "Section 2 content sec 2",
+      },
+    ],
+  },
+  {
+    "Page 3 title": [
+      {
+        "Section 1 tile": "Section 1 content",
+      },
+      {
+        "Section 2 tile": "Section 2 content sec 2",
+      },
+    ],
+  },
+  {
+    "Page 4 title": [
+      {
+        "Section 4 tile": "Section 1 content",
+      },
+      {
+        "Section 2 tile": "Section 2 content sec 2",
       },
     ],
   },
 ];
 
-var pages = [];
-var cont = [];
-var subcont = [];
-var matches = {};
-
-for (var i in pageData) {
-  var pageKey = Object.keys(pageData[i])[0];
-  console.log(pageKey);
-  for (var j in pageData[i][pageKey]) {
-    var sectionKey = Object.keys(pageData[i][pageKey][j])[0];
-    console.log(sectionKey);
-    console.log(pageData[i][pageKey][j][sectionKey]);
-  }
-}
+var sectionContentArray = []; // array to hold section content (Cannot make it as key value pair as there may be multiple sections with same content)
+var sectionContentMap = []; // [{pageTitle: sectionTitle}] index corresponding to sectionContentArray
+var matches = {}; // searchquery matched sectionContent stored in the form - {pageTitle: [{sectionTitle: sectionContent}]}
 
 $(document).ready(function () {
   for (var i in pageData) {
-    var pageKey = Object.keys(pageData[i])[0];
-    var htmlData = `<h1>${pageKey}</h1>`;
-    for (var j in pageData[i][pageKey]) {
-      var sectionKey = Object.keys(pageData[i][pageKey][j])[0];
-      htmlData += `<h2>${sectionKey}</h2>`;
-      htmlData += `<p>${pageData[i][pageKey][j][sectionKey]}</p>`;
-      cont.push(pageData[i][pageKey][j][sectionKey]);
-      subcont.push({ [pageKey]: sectionKey });
+    var pageTitle = Object.keys(pageData[i])[0];
+    for (var j in pageData[i][pageTitle]) {
+      var sectionTitle = Object.keys(pageData[i][pageTitle][j])[0];
+      var sectionContent = pageData[i][pageTitle][j][sectionTitle];
+      sectionContentArray.push(sectionContent);
+      sectionContentMap.push({ [pageTitle]: sectionTitle });
     }
-    pages.push({ [pageKey]: htmlData });
   }
 
   function element(id) {
     return document.getElementById(id);
   }
+
   let allSearchData = ""; //decleared to collect all search names
 
-  //gets each inputs data starting from second input
-  function getResults() {
-    //gets value of input
+  function getHighlightedData(searchStr, str) {
+    var searchStrLen = searchStr.length;
+    var updatedStr = "";
+    if (searchStrLen == 0) {
+      return [];
+    }
+    var startIndex, index, lowerStr, lowerSearchStr;
+    startIndex = 0;
+    lowerStr = str.toLowerCase();
+    lowerSearchStr = searchStr.toLowerCase();
+    while ((index = lowerStr.indexOf(lowerSearchStr, startIndex)) > -1) {
+      updatedStr +=
+        str.slice(startIndex, index) +
+        "<mark>" +
+        str.slice(index, index + searchStrLen) +
+        "</mark>";
+      startIndex = index + searchStrLen;
+    }
+    updatedStr += str.slice(startIndex);
+    return updatedStr;
+  }
+
+  function getContentList() {
     let search = element("search-input").value;
     allSearchData = ""; //clears data for each word typed
-    matches = {}; //clears matches for each word typed
-
-    hideSearchResults();
-    clearSearchResults();
-    clearSearchData(); //
-    //starts searching from the second input
-    if (search.length > 1) {
-      let counter = 0; // counts to 10
-      for (var i = 0; i < cont.length; i++) {
-        // if (counter < 10) {
-        //   var x = cont[i];
-        //   //checks for similarities
-        //   if (x.toLowerCase().includes(search.toLowerCase())) {
-        //     //populates the suggestion div
-        //     element("search-results").innerHTML +=
-        //       "<div class='search-item' onclick='displayData(\"" +
-        //       x +
-        //       "\")'><p>" +
-        //       x +
-        //       "</p></div>";
-
-        //     counter++;
-        //   }
-        // }
-        if (cont[i].toLowerCase().includes(search.toLowerCase())) {
-          //saves all the realated names
-          var pageTitle = Object.keys(subcont[i])[0];
-          if (typeof matches[pageTitle] !== "undefined") {
-            matches[pageTitle].push({ [subcont[i][pageTitle]]: cont[i] });
-            // allSearchData +=
-            //   "<div id='pageid' onclick='getSectionList(\"" +
-            //   pageTitle +
-            //   "\")'><p>" +
-            //   pageTitle +
-            //   "</p></div>";
-
-            allSearchData +=
-              "<div class='pageid' id='" +
-              pageTitle +
-              "'><p><u>" +
-              pageTitle +
-              "</u></p></div>";
-          } else matches[pageTitle] = [{ [subcont[i][pageTitle]]: cont[i] }];
-        }
-      }
-      // element("search-data").innerHTML = allSearchData;
-
-      // var ary = Array.prototype.slice.call(
-      //   document.querySelectorAll(".pageid")
-      // );
-      // ary.forEach(function (el) {
-      //   // Callbacks are passed a reference to the event object that triggered the handler
-      //   el.addEventListener("click", function (evt) {
-      //     // The this keyword will refer to the element that was clicked
-      //     console.log(this.id, el);
-      //   });
-      // });
-      console.log(matches);
-      displaySearchResults();
-    }
+    var i = this.getAttribute("idx");
+    var pageTitle = this.getAttribute("pgid");
+    var sectionTitle = this.id;
+    var sectionContent = matches[pageTitle][i][sectionTitle];
+    var content = getHighlightedData(search, sectionContent);
+    allSearchData += "<div class='contentid'><p>" + content + "</p></div>";
+    displayAllData(allSearchData);
   }
 
-  //displays the suggestion div
-  function displaySearchResults() {
-    element("search-results").style.display = "block";
-  }
-  //clears the suggestion div
-  function clearSearchResults() {
-    element("search-results").innerHTML = "";
-  }
-
-  //hides the suggestion div
-  function hideSearchResults() {
-    element("search-results").style.display = "none";
-  }
-  //displays names when you click a suggestions
-  function displayData(name) {
-    element("search-data").innerHTML = "<p>" + name + "</p>";
-    hideSearchResults();
-  }
-
-  function getSectionList(zEvent) {
+  function getSectionList() {
     allSearchData = ""; //clears data for each word typed
-    console.log("here");
     var pageTitle = this.id;
-    console.log(pageTitle);
     for (var i = 0; i < matches[pageTitle].length; i++) {
       var sectionTitle = Object.keys(matches[pageTitle][i])[0];
       allSearchData +=
@@ -161,74 +115,65 @@ $(document).ready(function () {
     }
     displayAllData(allSearchData);
   }
-  function getHighlightedData(searchStr, str) {
-    var searchStrLen = searchStr.length;
-    var updatedStr = "";
-    if (searchStrLen == 0) {
-      return [];
-    }
-    var startIndex = 0,
-      index,
-      indices = [];
-    while (
-      (index = str.toLowerCase().indexOf(searchStr.toLowerCase(), startIndex)) >
-      -1
-    ) {
-      indices.push(index);
-      updatedStr +=
-        str.slice(startIndex, index) +
-        "<mark>" +
-        str.slice(index, index + searchStrLen) +
-        "</mark>";
-      startIndex = index + searchStrLen;
-    }
-    updatedStr += str.slice(startIndex);
-    return updatedStr;
-  }
-  function getContentList(zEvent) {
-    let search = element("search-input").value;
+
+  function getPageList() {
+    let search = element("search-input").value; //gets value of input
     allSearchData = ""; //clears data for each word typed
-    console.log("here2");
-    var sectionTitle = this.id;
-    var pageTitle = this.getAttribute("pgid");
-    console.log(sectionTitle + " " + pageTitle + " " + search);
-    var i = this.getAttribute("idx");
-    var content = getHighlightedData(
-      search,
-      matches[pageTitle][i][sectionTitle]
-    );
-    allSearchData += "<div class='contentid'><p>" + content + "</p></div>";
-    displayAllData(allSearchData);
+    matches = {}; //clears matches for each word typed
+
+    clearSearchData(); //clears previous search results
+
+    //starts searching for matches
+    if (search.length >= 1) {
+      for (var i = 0; i < sectionContentArray.length; i++) {
+        if (
+          sectionContentArray[i].toLowerCase().includes(search.toLowerCase())
+        ) {
+          //saves all matching contents
+          var pageTitle = Object.keys(sectionContentMap[i])[0];
+          var sectionTitle = sectionContentMap[i][pageTitle];
+          var sectionContent = sectionContentArray[i];
+
+          //if page title is in matches
+          if (typeof matches[pageTitle] !== "undefined")
+            matches[pageTitle].push({ [sectionTitle]: sectionContent });
+          else {
+            matches[pageTitle] = [{ [sectionTitle]: sectionContent }];
+            //inserting page title in the list
+            allSearchData +=
+              "<div class='pageid' id='" +
+              pageTitle +
+              "'><p><u>" +
+              pageTitle +
+              "</u></p></div>";
+          }
+        }
+      }
+    }
   }
 
   //displays all related names to your search when you hit enter
   function displayAllData(names) {
     element("search-data").innerHTML = names;
-    hideSearchResults();
-    $(".pageid").click(getSectionList);
-    $(".sectionid").click(getContentList);
+    $(".pageid").click(getSectionList); //binds click event to page titles
+    $(".sectionid").click(getContentList); //binds click event to section titles
   }
+
   //clears names displayed from search result
   function clearSearchData() {
     element("search-data").innerHTML = "";
   }
+
   //gets results after each input
   element("search-input").oninput = function () {
-    getResults();
+    getPageList();
   };
-
-  // element("pageid").addEventListener("click", function () {
-  //   console.log("here");
-  //   getSectionList(pageTitle);
-  // });
 
   element("search-input").addEventListener("keyup", function (event) {
     // Number 13 is the "Enter" key on the keyboard
     if (event.keyCode === 13) {
-      // Cancel the default action, if needed
-      event.preventDefault();
-      // Trigger the button element with a click
-      displayAllData(allSearchData);
+      event.preventDefault(); // Cancel the default action, if needed
+      displayAllData(allSearchData); // Trigger the button element with a click
     }
   });
 });
